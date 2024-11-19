5e6ce02f29f67ae4e497eb4806c44c9772ecd0f2
MANISH GUPTA <kmgupta822000@gmail.com>
2024-11-19 21:01:53 +0530

    newsapp

---
+++ src/app/data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(protected http: HttpClient) { }
+  getNewsArticle(options:any){
+    return this.http.get('https://newsapi.org/v2/everything?q=all&language=hi&sortBy=publishedAt&apiKey=77ad79148aa647c9bdec33173ad2d7c2');
+  }
+}
